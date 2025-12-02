7d1d22252d175d9f33c400f7f4100c794d6c95e6
ashirabrar3214 <ashirabrar3214@gmail.com>
2025-12-02 15:38:16 -0600

    Initial commit with React app and welcome page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome!</h1>
+        <p>Welcome to our React application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
